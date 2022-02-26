739b5e3137fa2a2400a45e4e31076b8fd4f22df0
Fernando Haring <manaxtatu@gmail.com>
2022-02-25 23:40:15 -0300

    Functional Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+
+    def __str__(self):
+        # Self hace referencia a la clase Post
+        return self.title
